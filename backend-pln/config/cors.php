3ee9363aeaec61e3b17d9a904286057ff6f4e185
mhmdalhaqiii <mhmd.alhaqiii@gmail.com>
2026-05-25 18:26:31 +0700

    Create cors.php

---
+++ backend-pln/config/cors.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+    ],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://localhost:3000',
+        'https://test-deployment-app-mu.vercel.app',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
